8817e8fc0f69b7c09989534318540190c5101d6b
lu.qianyu <lqygo1234@qq.com>
2018-04-25 15:23:31 +0800

    微信服务器

--- server/api/wx-receiver.js
+++ server/api/wx-receiver.js
@@ -5,18 +5,35 @@ var _ = require('underscore'),
 import lo from 'lodash';
 
 var fs = require("fs")
+var crypto = require('crypto');
+const hash = crypto.createHash('sha1')
 
 import apiAuth from '../middleware/auth/api-auth'
 
 
 var wxReceiver = function(req, res, next) {
-    console.log(req.body);
-    resProcessor.jsonp(req, res, {
-        state: { code: 0 },
-        data: {
-            sysTime: new Date().getTime()
-        }
-    });
+    // console.log('wxReceiver-body: ', req.body);
+
+    const signature = req.query.signature
+    const timestamp = req.query.timestamp
+    const nonce = req.query.nonce
+    const echostr = req.query.echostr
+
+    const arr = [nonce, 'njjnjn', timestamp]
+    arr.sort()
+
+    const tStr = arr[0] + arr[1] + arr[2]
+    hash.update(tStr)
+
+    console.log(hash.digest('hex'));
+
+    if(hash.digest('hex') == signature) {
+        // 这是来自微信官方的消息
+        console.log('wxReceiver-body: ', req.body);
+    }
+
+
+    res.send(echostr)
 };
 
 
