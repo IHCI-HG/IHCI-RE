27762f26236d0e54cca916df3fedc521efd84e69
zcc <643033786@qq.com>
2018-07-04 10:36:35 +0800

    fix mail

--- server/components/mail/notificationMail.js
+++ server/components/mail/notificationMail.js
@@ -51,15 +51,14 @@ export const notificationMail = async function(userIdList, topicObj, type) {
                 subject: '消息提醒',
                 // 收件人
                 to: item.personInfo.mail, 
-                // text: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> \
-                //     <html xmlns="http://www.w3.org/1999/xhtml"><body> \
-                //        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\
-                //        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-                //        <table border="1" cellpadding="0" cellspacing="0" width="100%">\
-                //        <tbody><tr><td><font color="#333300" style="font-weight: bold;">'+topicObj.creator.name+'</font>'+type+':&nbsp; '+topicObj.title+'</td> </tr>\
-                //        <tr><td><b>时间</b>：'+formatDate(new Date())+'</td></tr>\
-                //        <tr><td><b>内容</b>：'+topicObj.content+'</td></tr></tbody></table></body></html>'                                    
-                 text:  topicObj.creator.name+' '+type+':  '+topicObj.title+'        时间：'+formatDate(new Date())+'         内容：'+topicObj.content
+                html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> \
+                       <html xmlns="http://www.w3.org/1999/xhtml"><head> \
+                       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\
+                       <meta name="viewport" content="width=device-width, initial-scale=1.0"></head>\
+                       <body><table border="1" cellpadding="0" cellspacing="0">\
+                       <tbody><tr><td><b>'+topicObj.creator.name+'</b>'+type+':&nbsp; '+topicObj.title+'</td> </tr>\
+                       <tr><td><b>时间</b>：'+formatDate(new Date())+'</td></tr>\
+                       <tr><td><b>内容</b>：'+topicObj.content+'</td></tr></tbody></table></body></html>'                                    
             };
             try{ 
                 const sendFlag = await sendMail(mail)
