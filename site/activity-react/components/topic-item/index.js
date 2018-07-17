6b75bf64159f7c40eea91ef08c3a4d4fb2d1ff0a
liangaas <1157732308@qq.com>
2018-07-17 15:10:59 +0800

    显示评论数，和点击评论按钮进入评论页面

--- site/activity-react/components/topic-item/index.js
+++ site/activity-react/components/topic-item/index.js
@@ -1,11 +1,31 @@
 import * as React from 'react';
+import api from '../../utils/api';
 import { timeBefore, createMarkup } from '../../utils/util'
 import './style.scss'
 
 class TopicItem extends React.PureComponent {
+    state={
+        discussList:[]   
+    }
+
+    componentDidMount = async() => {
+        this.initPageInfo()
+    }
+    initPageInfo = async () => {
+        const result = await api('/api/topic/get', {
+            method: 'GET',
+            body: {
+                topicId: this.props._id
+            }
+        })
+        const discussList = result.data.discussList
+        this.setState({
+            discussList:discussList
+        })
+    }
     render() {
         return (
-            <div className="topic-item" key={"topic-item-" + this.props._id} onClick={() => { this.props.locationTo('/discuss/topic/' + this.props._id) }}>
+            <div className="topic-item" key={"topic-item-" + this.props._id} >
                 <img src={this.props.creator.headImg} alt="" className="head-img" />
                 <div className="name">{this.props.creator.name}</div>
                 <div className="main">
@@ -15,7 +35,10 @@ class TopicItem extends React.PureComponent {
                 {this.props.fileList.length > 0 &&
                     <i className="icon iconfont time">&#xe6dd;</i>
                 }
+                <div onClick={() => { this.props.locationTo('/discuss/topic/' + this.props._id) }}>评论 {this.state.discussList
+                .length}</div>
                 <div className="time">{timeBefore(this.props.create_time)}</div>
+            
             </div>
         )
     }
