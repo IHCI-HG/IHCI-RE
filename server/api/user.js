fc325f6c0585f426249f2fd098c65adae6c3e2ca
linyang <291192462@qq.com>
2018-08-07 16:33:02 +0800

    绑定解绑逻辑完善

--- server/api/user.js
+++ server/api/user.js
@@ -147,10 +147,14 @@ const loginAndBindWx = async (req, res, next) => {
     const result1 = (await UserDB.findByUsername(username)).toObject()
     result1.username = username
     result1.password = password
+    result1.unionid = unionid
+    result1.oldId = result1._id.toString()
     delete result1._id
     if(result) {
         await UserDB.delUserByUsername(username)
         const findResult = await UserDB.findByUnionId(unionid)
+        result1.noticeList = [...result1.noticeList, ...findResult.noticeList]
+        result1.teamList = [...result1.teamList, ...findResult.teamList]
         const userId = findResult._id
         const userDBresult = await UserDB.updateUser(userId, result1)
         req.rSession.userId = findResult._id
