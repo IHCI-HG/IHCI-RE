acf2cfa1faff0607402c70eebc73fb9b8ff19b19
梁奕菡 <liangyihan@liangyihandeMacBook-Pro.local>
2018-11-07 09:27:36 +0800

    首页样式

--- site/activity-react/pages/main/index.js
+++ site/activity-react/pages/main/index.js
@@ -66,32 +66,51 @@ export default class MainPage extends React.Component {
                     <LoginView showWxDialogHandle={this.showWxDialogHandle}/>
                 </div>
             </div>
-            <div className="video">
-                <div className="video-des">这是一些关于视频的描述balabala这是一些关于视频的描述balabala这是一些关于视频的描述balabala这是一些关于视频的描述balabala</div>
+            <div className="video">  
                 <div className="video-wrap">
                     { /* <iframe frameborder="0" width="450" height="254" src="https://v.qq.com/iframe/player.html?vid=f0564fwe5va&tiny=0&auto=0" allowfullscreen></iframe> */}
+                    <div><img className="video-template" src={require('./mp4.png')}/></div>
+                    <div className="video-des">这是一些关于视频的描述balabala</div>
                 </div>
+                
             </div>
             <div className="stories">
-                <h1>iHCI stories</h1>
                 <div className="story-con">
                     <div className="story-item">
-                        <div className="head-img"></div>
-                        <div className="name">名字</div>
-                        <div className="title">这是头衔的描述</div>
-                        <div className="desc">一段说明性的文字</div>
+                        <div><img  className="head-img" src={require('./toxiang1.png')}/></div>
+                        <div className="item-wrap">
+                        <div className="name">Meta Hirschl</div>
+                        <div className="title">网页设计师</div>
+                        <div className="desc">
+                        <div>我是一个待业者</div>
+                        <div>通过人机交互实验室</div>
+                        <div>我获得了一份网页设计师的工作</div>
+                        </div>
+                        </div>
                     </div>
                     <div className="story-item">
-                        <div className="head-img"></div>
-                        <div className="name">名字</div>
-                        <div className="title">这是头衔的描述</div>
-                        <div className="desc">一段说明性的文字</div>
+                        <div><img  className="head-img" src={require('./toxiang2.png')}/></div>
+                        <div className="item-wrap">
+                        <div className="name">Brian Grant</div>
+                        <div className="title">出国考研学生</div>
+                        <div className="desc">
+                        <div>我是一个在国外考研的大学生</div>
+                        <div>通过人机交互实验室</div>
+                        <div>我在国外考研更加轻松了</div>
+                        </div>
+                        </div>
                     </div>
                     <div className="story-item">
-                        <div className="head-img"></div>
-                        <div className="name">名字</div>
-                        <div className="title">这是头衔的描述</div>
-                        <div className="desc">一段说明性的文字</div>
+                        <div><img className="head-img" src={require('./toxiang3.png')}/></div>
+                        <div className="item-wrap">
+                        <div className="name">Maxim Orlov</div>
+                        <div className="title">创业者</div>
+                        <div className="desc">
+                        <div>我是创业者</div>
+                        <div>通过人机交互实验室</div>
+                        <div>让我的公司开发了更好的产品</div>
+                        </div>
+                        </div>
                     </div>
                 </div>
 
@@ -108,20 +127,16 @@ export default class MainPage extends React.Component {
                         <div href="">关于我们</div>
                     </div>
                     <div className="foot-item">
-                        <div className="foot-item-title">分类</div>
-                        <div href="">软件工程</div>
-                        <div href="">人机交互</div>
-                        <div href="">人工智能</div>
-                    </div>
-                    <div className="foot-item">
-                        <div className="foot-item-title">编程课程</div>
+                        <div className="foot-item-title">iHCI俱乐部</div>
                         <div href="">HTML&CSS</div>
                         <div href="">JavaScript</div>
                         <div href="">Python</div>
                     </div>
                     <div className="foot-item">
-                        <div className="foot-item-title">资源</div>
-                        <div href="">参考链接</div>
+                        <div className="foot-item-title">iHCI实验室</div>
+                        <div href="">软件工程</div>
+                        <div href="">人机交互</div>
+                        <div href="">人工智能</div>
                     </div>
                 </div>
             </div>
